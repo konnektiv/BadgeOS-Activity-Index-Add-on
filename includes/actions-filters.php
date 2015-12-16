7e1526bf6bf7d1f3cdb51d1a0f186e1c8cfbd9d9
Christoph Herbst <chris.p.herbst@gmail.com>
2015-12-16 12:45:54 +0100

    output activity index per month
    also add some test data

--- includes/actions-filters.php
+++ includes/actions-filters.php
@@ -5,9 +5,7 @@ add_action('wp_dashboard_setup', function () {
 	wp_add_dashboard_widget('badgeos_activity_index_widget', __('Activity Index', 'badgeos-activity-index'), function () {
 		global $wpdb;
 		$meta_key = '_badgeos_achievements';
-		$since = 0;
 		$achievement_type = 'site-activity';
-
 
 		// get all achievements from all users
 		$user_achievements = $wpdb->get_col( $wpdb->prepare("
@@ -15,33 +13,49 @@ add_action('wp_dashboard_setup', function () {
 			FROM $wpdb->usermeta
 			WHERE meta_key = %s", $meta_key) );
 
-		$all_achievements = array();
+		$points = array();
+		$test_count = 100;
 
 		if ( is_array( $user_achievements) && ! empty( $user_achievements ) ) {
-			foreach ( $user_achievements as $ukey => $achievements ) {
+			foreach ( $user_achievements as $achievements ) {
 				$achievements = unserialize($achievements);
 				// XXX: Why index 1 here?
 				if ( is_array( $achievements) && ! empty( $achievements ) )
 					$achievements = $achievements[1];
 
 				if ( is_array( $achievements) && ! empty( $achievements ) ) {
-					foreach ( $achievements as $key => $achievement ) {
-						// Add any achievements earned after our since timestamp
-						// and that match our achievement type
-						if ( $since < $achievement->date_earned &&
-						   	 $achievement_type == $achievement->post_type )
-							$all_achievements[] = $achievement;
+
+					// add some random values
+					for($i = 0; $i< $test_count; $i++){
+						$achievements[] = json_decode(json_encode(array(
+							'post_type' => $achievement_type,
+							'date_earned' => rand(time() - 31536000, time()),
+							'points' => rand(1, 10),
+						)), FALSE);
+					}
+
+					foreach ( $achievements as $achievement ) {
+						// Add any achievements that match our achievement type
+						if ( $achievement_type == $achievement->post_type ) {
+							$key = strtotime(date("F Y", $achievement->date_earned));
+
+							if (!isset($points[$key]))
+								$points[$key] = 0;
+							$points[$key] += intval($achievement->points);
+						}
 					}
 				}
 			}
 		}
 
-		// sum activity index
-		$points = array_reduce($all_achievements, function($carry, $achievement){
-			return $carry + intval($achievement->points);
-		}, 0);
-
-		echo "Current Activity Index: $points";
+		ksort($points);
+		$points = array_reverse($points, true);
+		echo "<table><thead><tr><th>Month</th><th>Index</th></thead><tbody>";
+		foreach ( $points as $key => $point ) {
+			$date = date("F Y", $key);
+			echo "<tr><td>$date</td><td>$point</td></tr>";
+		}
+		echo "</tbody></table>";
 	});
 });
 
